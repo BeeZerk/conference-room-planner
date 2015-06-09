e6582c8762ddd0f2fa4febb1677bba45e0641208
Bee <bee@netempire.de>
2015-06-09 15:43:06 +0200

    fix migration

--- db/migrate/20150528131017_create_rooms.rb
+++ db/migrate/20150528131017_create_rooms.rb
@@ -2,7 +2,6 @@ class CreateRooms < ActiveRecord::Migration
   def change
     create_table :rooms do |t|
       t.string :name
-      t.references :attachment, index: true, foreign_key: true
       t.string :shortcut
 
       t.timestamps null: false
