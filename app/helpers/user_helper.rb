d4940b6620466dff9e96125f285c5181660c3a7b
bee <bemigho95@hotmail.de>
2015-08-23 09:33:59 -0700

    notificiations and friendship implemented | not finished

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,11 @@
 module UserHelper
 
   def count_unchecked_notifications
-    Notification.where(user_id: current_user.id, seen: false).count
+
+    count = Notification.where(user_id: current_user.id, seen: false).count
+    if count != 0
+      return "<div class=\"label label-danger\">#{count}</div>".html_safe
+    end
+
   end
 end
